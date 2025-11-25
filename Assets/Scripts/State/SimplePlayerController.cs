3add1ae6bdc6635261e152bde813fbc2f4c28402
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-11-25 22:51:26 +0900

    [update]playercontrollerとpaleyrmoverの接続

--- Assets/Scripts/State/SimplePlayerController.cs
+++ Assets/Scripts/State/SimplePlayerController.cs
@@ -4,29 +4,50 @@ using UnityEngine.InputSystem;
 
 public class SimplePlayerController : MonoBehaviour
 {
-    private InputBuffer _InputBuffer;
-    private SimplePlayerMover _PlayerMover;
+    private InputBuffer _inputBuffer;
+    private SimplePlayerMover _playerMover;
+    private Rigidbody _rb;
     private void Awake()
     {
-        _InputBuffer = GetComponent<InputBuffer>();
-        _PlayerMover = GetComponent<SimplePlayerMover>();
+        _inputBuffer = GetComponent<InputBuffer>();
+        _playerMover = GetComponent<SimplePlayerMover>();
+        _rb = GetComponent<Rigidbody>();
+        _playerMover =new SimplePlayerMover(_rb,Camera.main.transform);
     }
 
     private void Start()
     {
-        _InputBuffer.MoveAction.performed += OnMoveAction;
-        _InputBuffer.MoveAction.canceled += OnMoveAction;
+        _inputBuffer.MoveAction.performed += OnMoveAction;
+        _inputBuffer.MoveAction.canceled += OnMoveAction;
     }
     
 
     private void OnDestroy()
     {
-        _InputBuffer.MoveAction.performed -= OnMoveAction;
-        _InputBuffer.MoveAction.canceled -= OnMoveAction;
+        _inputBuffer.MoveAction.performed -= OnMoveAction;
+        _inputBuffer.MoveAction.canceled -= OnMoveAction;
+    }
+
+    private void Update()
+    {
+        _playerMover.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        _playerMover.FixedUpdate();
     }
 
     private void OnMoveAction(InputAction.CallbackContext context)
     {
-        
+        Vector2 input = context.ReadValue<Vector2>();
+        if (context.performed)
+        {
+            _playerMover.SetMoveInput(input);
+        }
+        else if (context.canceled)
+        {
+            _playerMover.MoveStop();
+        }
     }
 }

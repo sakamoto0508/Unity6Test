f6a152291020d42a520fa309a85dd8d71fd08ab9
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-11-27 00:35:01 +0900

    [add]enemycontroller

---
+++ Assets/Scripts/State/EnemyController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private GoapAgent _goapAgent;
+    private StateMachine _stateMachine;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _goapAgent = GetComponent<GoapAgent>();
+        _stateMachine = GetComponent<StateMachine>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        _stateMachine.ChangeState(new IdleState());
+    }
+
+    private void Update()
+    {
+        _goapAgent.UpdateAgent();
+    }
+
+    private void MoveTo(Vector3 point)
+    {
+        _stateMachine.ChangeState(new MoveState(_navMeshAgent, point));
+    }
+}
